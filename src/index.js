98f6bfc7b03ba5d1c22ceabc4a8763b2383f2ad1
wtwerner <wtwerner@gmail.com>
2021-02-07 22:43:54 -0600

    Move users reducer

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-
-const users = () => []
+import users from './reducers/users.js'
 
 const reducer = combineReducers({
   users
