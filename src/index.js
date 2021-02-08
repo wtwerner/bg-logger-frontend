807eae8a4b69dd8c39895553c9ed0ecf9b961b55
wtwerner <wtwerner@gmail.com>
2021-02-07 22:49:49 -0600

    Move store

--- src/index.js
+++ src/index.js
@@ -3,16 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import users from './reducers/users.js'
-
-const reducer = combineReducers({
-  users
-})
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+import store from './store'
 
 ReactDOM.render(
   <Provider store={ store }>
---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import users from './reducers/users.js'
+
+const reducer = combineReducers({
+  users
+})
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+export default store
