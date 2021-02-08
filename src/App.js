ed2beabe0dcd8f8b4b3d76301fb0055ea5538d0f
wtwerner <wtwerner@gmail.com>
2021-02-08 09:09:32 -0600

    Add Bootstrap CDN

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
 
 function App() {
   return (
-    "Hello"
+    <div>
+      <Container>
+        <Jumbotron>
+          <h1>Hello, world!</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
+          </p>
+          <p>
+            <Button variant="primary">Learn more</Button>
+          </p>
+        </Jumbotron>
+      </Container>
+    </div>
   );
 }
 
