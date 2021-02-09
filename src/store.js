bd6d55c96ae0cbcf61195a90e3f2b46216bbcd69
wtwerner <wtwerner@gmail.com>
2021-02-09 09:41:10 -0600

    Remove users reducer, re-configue thunk

--- src/store.js
+++ src/store.js
@@ -1,16 +1,15 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import users from './reducers/users.js';
 import currentUser from './reducers/currentUser.js';
 import loginForm from './reducers/loginForm.js'
 
 const reducer = combineReducers({
-  users,
   currentUser,
   loginForm
 })
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 export default store
