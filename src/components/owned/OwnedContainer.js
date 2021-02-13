5c9dc64ac399c5fc8a6f5d29705625c5de5cbcd0
wtwerner <wtwerner@gmail.com>
2021-02-12 22:51:36 -0600

    Create owned table component

--- src/components/owned/OwnedContainer.js
+++ src/components/owned/OwnedContainer.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
+import OwnedTable from './OwnedTable'
+import { connect } from 'react-redux'
 
-const OwnedContainer = () => {
-    return (
-        <Container>
-            <h1>OWNED</h1>
-        </Container>
-    )
+class OwnedContainer extends React.Component {
+    render() {
+        return (
+            <Container>
+                <OwnedTable user={this.props.currentUser} />
+            </Container>
+        )
+    }
 }
 
-export default OwnedContainer
+const mapStateToProps = ({ currentUser }) => {
+    return {
+        currentUser
+    }
+}
+
+export default connect(mapStateToProps)(OwnedContainer)
