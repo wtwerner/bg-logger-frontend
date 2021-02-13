2893b73812bc9e767816e2124323d2b484c7b9a4
wtwerner <wtwerner@gmail.com>
2021-02-12 20:42:54 -0600

    Install font awesome and style table

--- src/components/discover/DiscoverTable.js
+++ src/components/discover/DiscoverTable.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Table, Button } from 'react-bootstrap'
+import { FaStar, FaArrowAltCircleDown } from 'react-icons/fa';
 
 class DiscoverTable extends React.Component {
     render() {
         return (
-            <Table striped bordered hover>
+            <Table striped bordered hover className="pt-10">
                 <thead>
                     <tr>
                         <th>Image</th>
@@ -20,15 +21,15 @@ class DiscoverTable extends React.Component {
                 <tbody>
                     {this.props.games.map(game => {
                         if (game.visits > 100)
-                            return (<tr key={game.id}>
+                            return (<tr key={game.id} className="align-middle" >
                                 <td><img src={game.images.small} alt="game" /></td>
                                 <td>{game.name}</td>
                                 <td>{game.rank < 1000 ? game.rank : 'N/A'}</td>
                                 <td>{game.average_learning_complexity.toFixed(2)}</td>
                                 <td>{game.min_players}-{game.max_players}</td>
                                 <td>{'$'+(game.price > 0.01 ? game.price : 'N/A')}</td>
-                                <td><Button /></td>
-                                <td><Button /></td>
+                                <td className="text-center"><Button><FaStar /></Button></td>
+                                <td className="text-center"><Button><FaArrowAltCircleDown /></Button></td>
                             </tr>)    
                         }
                     )}
