09850c5d4dd3629f8981ed97c490fbd59876094b
wtwerner <wtwerner@gmail.com>
2021-02-15 20:26:37 -0600

    Add button to owned table to remove from owned

--- src/components/owned/OwnedTable.js
+++ src/components/owned/OwnedTable.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Table, Button } from 'react-bootstrap'
+import OwnedButton from '../buttons/OwnedButton'
 
 class OwnedTable extends React.Component {
     render() {
@@ -13,18 +14,20 @@ class OwnedTable extends React.Component {
                         <th>Players</th>
                         <th>Add a Play</th>
                         <th>View Plays</th>
+                        <th>Remove from Owned</th>
                     </tr>
                 </thead>
                 <tbody>
                     {this.props.userGames.map(game => {
                             return (
-                                <tr key={game.id} className="align-middle" >
+                                <tr key={game.id} game_id={game.id} className="align-middle" >
                                     <td className="text-center"><img src={game.images.small} alt="game" /></td>
                                     <td className="text-center">{game.name}</td>
                                     <td className="text-center">{game.rank}</td>
                                     <td className="text-center">{game.min_players}-{game.max_players}</td>
                                     <td className="text-center"><Button>Add a Play</Button></td>
                                     <td className="text-center"><Button>View Plays</Button></td>
+                                    <td className="text-center"><OwnedButton game_id={game.id} /></td>
                                 </tr>
                             )}
                     )}
