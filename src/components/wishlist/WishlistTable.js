d9d924681be08914d356f0be6cecbe783b5c12ad
wtwerner <wtwerner@gmail.com>
2021-02-18 21:28:54 -0600

    Send game instead of game_id to WishlistButton

--- src/components/wishlist/WishlistTable.js
+++ src/components/wishlist/WishlistTable.js
@@ -26,7 +26,7 @@ class WishlistTable extends React.Component {
                                     <td className="text-center">{game.rank}</td>
                                     <td className="text-center">{game.min_players}-{game.max_players}</td>
                                     <td className="text-center">
-                                        <WishlistButton game_id={game.id} />
+                                        <WishlistButton game={game} />
                                     </td>
                                     <td className="text-center">
                                         <WishlistToOwnedButton game={game} />
