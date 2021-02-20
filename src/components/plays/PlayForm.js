90e945cc240a048c9af00e46a078fe42a9b2699f
wtwerner <wtwerner@gmail.com>
2021-02-20 13:29:56 -0600

    Create playForm in Redux store

---
+++ src/components/plays/PlayForm.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { updateLoginForm } from "../../actions/loginForm.js"
+import { login } from "../../actions/currentUser.js"
+
+const PlayForm = ({ loginFormData, updateLoginForm, login }) => {
+    
+    const handleOnChange = event => {
+        const { name, value } = event.target
+        const updatedFormInfo = {
+            ...loginFormData,
+            [name]: value
+        }
+        updateLoginForm(updatedFormInfo)
+    }
+
+    const handleOnSubmit = event => {
+        event.preventDefault()
+        login(loginFormData)
+    }
+
+    return (
+        <form onSubmit={handleOnSubmit}>
+            <input value={loginFormData.email} name="email" type="text" onChange={handleOnChange} />
+            <input value={loginFormData.password} name="password" type="text" onChange={handleOnChange} />
+            <input value="Log In" type="submit" />
+        </form>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        playFormData: state.loginForm
+    }
+}
+
+export default connect(mapStateToProps, { updatePlayForm, recordPlay } )(PlayForm)
