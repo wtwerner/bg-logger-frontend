e101bf49469134b1e474193f7d7a2851b496dd9d
wtwerner <wtwerner@gmail.com>
2021-02-21 12:28:47 -0600

    Set max height and width for photos

--- src/components/discover/DiscoverTableRow.js
+++ src/components/discover/DiscoverTableRow.js
@@ -2,13 +2,15 @@ import React from 'react'
 import OwnedButton from '../buttons/OwnedButton'
 import WishlistButton from '../buttons/WishlistButton'
 
+import '../../photo_style.css'
+
 const DiscoverTableRow = (props) => {
     return (
         props.games.map(game => {
             if (game.visits > 100) {
                 return (
                     <tr key={game.id} game_id={game.id} className="align-middle" >
-                        <td className="text-center"><img src={game.images.small} alt="game" /></td>
+                        <td className="text-center"><img src={game.images.small} className="photo" alt="game" /></td>
                         <td className="text-center">{game.name}</td>
                         <td className="text-center">{game.rank < 1000 ? game.rank : 'N/A'}</td>
                         <td className="text-center">{game.min_players}-{game.max_players}</td>
--- src/components/owned/OwnedTableRow.js
+++ src/components/owned/OwnedTableRow.js
@@ -2,12 +2,14 @@ import React from 'react'
 import OwnedButton from '../buttons/OwnedButton'
 import { Button } from 'react-bootstrap'
 
+import '../../photo_style.css'
+
 const OwnedTableRow = (props) => {
     return (
         props.userGames.map(game => {
             return (
                 <tr key={game.id} game_id={game.id} className="align-middle" >
-                    <td className="text-center"><img src={game.images.small} alt="game" /></td>
+                    <td className="text-center"><img src={game.images.small} className="photo" alt="game" /></td>
                     <td className="text-center">{game.name}</td>
                     <td className="text-center">{game.rank}</td>
                     <td className="text-center">{game.min_players}-{game.max_players}</td>
