a75a6edbee40a6f827034795a0d718d52bfda9b9
wtwerner <wtwerner@gmail.com>
2021-02-21 19:27:09 -0600

    Set max photo size for wishlist

--- src/components/wishlist/WishlistTableRow.js
+++ src/components/wishlist/WishlistTableRow.js
@@ -2,12 +2,14 @@ import React from 'react'
 import WishlistButton from '../buttons/WishlistButton'
 import WishlistToOwnedButton from '../buttons/WishlistToOwnedButton'
 
+import '../../photo_style.css'
+
 const WishlistTableRow = (props) => {
     return (
         props.userGames.map(game => {
             return (
                 <tr key={game.id} game_id={game.id} className="align-middle" >
-                    <td className="text-center"><img src={game.images.small} alt="game" /></td>
+                    <td className="text-center"><img src={game.images.small} className="photo" alt="game" /></td>
                     <td className="text-center">{game.name}</td>
                     <td className="text-center">{game.rank}</td>
                     <td className="text-center">{game.min_players}-{game.max_players}</td>
