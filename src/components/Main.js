c64210600b0455c9043cbe9c4d680d8b7176d962
wtwerner <wtwerner@gmail.com>
2021-02-21 20:34:47 -0600

    Adjust main page jumbotron

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,15 @@
 import React from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 
 const Main = () => {
 
     return (
-        <Container>
+        <Container style={{width: "500px"}}>
             <Jumbotron>
-                <h1>Hello, world!</h1>
+                <h1>Welcome to BG Logger</h1>
                 <p>
-                    This is a simple hero unit, a simple jumbotron-style component for calling
-                    extra attention to featured content or information.
-                </p>
-                <p>
-                    <Button variant="primary">Learn more</Button>
+                    A simple app to search for, stash, and log plays of board games.
                 </p>
             </Jumbotron>
         </Container>
