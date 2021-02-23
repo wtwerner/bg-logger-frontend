1f34303c55556751fc096070961ac385f73e22d2
wtwerner <wtwerner@gmail.com>
2021-02-23 09:29:08 -0600

    Remove local state from modal

--- src/components/modals/modal.js
+++ src/components/modals/modal.js
@@ -3,26 +3,6 @@ import Modal from 'react-bootstrap/Modal'
 import './modal.css'
 
 export default class GameModal extends React.Component {
-
-    state={ 
-        name: null,
-        image: null,
-        rank: null,
-        id: null,
-        price: null,
-        year_published: null,
-        min_players: null,
-        max_players: null,
-        min_playtime: null,
-        max_playtime: null,
-        description: null,
-        primary_designer: null,
-        rules: null,
-        average_user_rating: null,
-        num_user_ratings: null,
-        bgg_id: null   
-    }   
-
     render(){
         return(
             <Modal 
