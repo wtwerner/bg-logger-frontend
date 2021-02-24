560124a598c0ed2052f5e4468794e74062699426
wtwerner <wtwerner@gmail.com>
2021-02-24 09:14:12 -0600

    Add grid to modal

--- src/components/modals/modal.js
+++ src/components/modals/modal.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import Modal from 'react-bootstrap/Modal'
+import { Modal, Container, Row, Col } from 'react-bootstrap'
 import './modal.css'
+import '../../photo_style.css'
+import PlayButton from '../buttons/PlayButton'
+import OwnedButton from '../buttons/OwnedButton'
 
 export default class GameModal extends React.Component {
     render(){
@@ -12,13 +15,33 @@ export default class GameModal extends React.Component {
                 aria-labelledby="example-custom-modal-styling-title"
             >
                 <Modal.Header closeButton>
-                    <Modal.Title>{this.props.game.name} ({this.props.game.year_published}) - Designed by: {this.props.game.primary_designer.name}</Modal.Title>
+                    <Modal.Title>{this.props.game.name} ({this.props.game.year_published}) - {this.props.game.primary_designer.name}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <p>{this.props.game.description_preview}</p>
+                    <Container>
+                        <Row>
+                            <Col xs={6} md={4}>
+                                <img src={this.props.game.images.small} className="photo" alt="game" />
+                            </Col>
+                            <Col xs={6} md={4}>
+                                Gameplay info
+                            </Col>
+                            <Col xs={6} md={4}>
+                                Game stats
+                            </Col>
+                        </Row>
+                        <br/>
+                        <Row>
+                            <Col>
+                                <h6>Description:</h6>
+                                <p>{this.props.game.description_preview}</p>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Modal.Body>
                 <Modal.Footer>
-
+                    <PlayButton />
+                    <OwnedButton game={this.props.game} />
                 </Modal.Footer>
             </Modal>
         )
