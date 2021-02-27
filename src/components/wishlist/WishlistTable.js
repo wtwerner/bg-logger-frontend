20eed9fde56f8788780863e60a2aed547da29a4a
wtwerner <wtwerner@gmail.com>
2021-02-27 08:59:02 -0600

    Add game modal to wishlist table

--- src/components/wishlist/WishlistTable.js
+++ src/components/wishlist/WishlistTable.js
@@ -1,25 +1,56 @@
 import React from 'react'
 import { Table } from 'react-bootstrap'
 import WishlistTableRow from './WishlistTableRow'
+import GameModal from '../modals/modal'
 
 class WishlistTable extends React.Component {
+
+    state = {
+        isOpen: false,
+        modalGame: null
+    }
+    
+    openModal = (game) => {
+        this.setState({ 
+            isOpen: true,
+            modalGame: game
+        });
+    }
+    
+    closeModal = () => this.setState({ isOpen: false });
+
     render() {
         return (
-            <Table striped hover className="pt-10">
-                <thead>
-                    <tr className="text-center">
-                        <th>Image</th>
-                        <th>Title</th>
-                        <th>Rank</th>
-                        <th>Players</th>
-                        <th>Remove</th>
-                        <th>Add to Owned</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <WishlistTableRow userGames={this.props.userGames} />
-                </tbody>
-            </Table>
+            <>
+                <Table striped hover className="pt-10">
+                    <thead>
+                        <tr className="text-center">
+                            <th>Image</th>
+                            <th>Title</th>
+                            <th>Rank</th>
+                            <th>Players</th>
+                            <th>More Info</th>
+                            <th>Remove</th>
+                            <th>Add to Owned</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.userGames.map(game => {
+                            return <WishlistTableRow openModal={this.openModal} game={game} key={game.id} />
+                        })}
+                    </tbody>
+                </Table>
+
+                {this.state.isOpen 
+                    ? <GameModal 
+                        closeModal={this.closeModal} 
+                        isOpen={this.state.isOpen} 
+                        handleSubmit={this.handleSubmit}
+                        game={this.state.modalGame}
+                        /> 
+                    : null 
+                }
+            </>
         )
     }   
 }
--- src/components/wishlist/WishlistTableRow.js
+++ src/components/wishlist/WishlistTableRow.js
@@ -1,28 +1,26 @@
 import React from 'react'
 import WishlistButton from '../buttons/WishlistButton'
 import WishlistToOwnedButton from '../buttons/WishlistToOwnedButton'
+import InfoButton from '../buttons/InfoButton'
 
 import '../../photo_style.css'
 
 const WishlistTableRow = (props) => {
     return (
-        props.userGames.map(game => {
-            return (
-                <tr key={game.id} game_id={game.id} className="align-middle" >
-                    <td className="text-center"><img src={game.images.small} className="photo" alt="game" /></td>
-                    <td className="text-center">{game.name}</td>
-                    <td className="text-center">{game.rank}</td>
-                    <td className="text-center">{game.min_players}-{game.max_players}</td>
-                    <td className="text-center">
-                        <WishlistButton game={game} />
-                    </td>
-                    <td className="text-center">
-                        <WishlistToOwnedButton game={game} />
-                    </td>
-                </tr>
-            )}
-        )
-    )
+        <tr key={props.game.id} game_id={props.game.id} className="align-middle" >
+            <td className="text-center"><img src={props.game.images.small} className="photo" alt="game" /></td>
+            <td className="text-center">{props.game.name}</td>
+            <td className="text-center">{props.game.rank}</td>
+            <td className="text-center">{props.game.min_players}-{props.game.max_players}</td>
+            <td className="text-center"><InfoButton game={props.game} openModal={props.openModal} /></td>
+            <td className="text-center">
+                <WishlistButton game={props.game} />
+            </td>
+            <td className="text-center">
+                <WishlistToOwnedButton game={props.game} />
+            </td>
+        </tr>
+    ) 
 }
 
 export default WishlistTableRow
