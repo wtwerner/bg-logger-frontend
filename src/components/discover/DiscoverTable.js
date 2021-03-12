ba666f231ecded2a34f28c14c4452f1a937c966a
wtwerner <wtwerner@gmail.com>
2021-03-11 19:02:54 -0600

    Add game modal to discover table

--- src/components/discover/DiscoverTable.js
+++ src/components/discover/DiscoverTable.js
@@ -1,26 +1,59 @@
 import React from 'react'
 import { Table } from 'react-bootstrap'
 import DiscoverTableRow from './DiscoverTableRow'
+import GameModal from '../modals/gameModal'
 
-const DiscoverTable = ({ games }) => {
-    return (
-        <Table striped hover className="pt-10">
-            <thead>
-                <tr className="text-center">
-                    <th>Image</th>
-                    <th>Title</th>
-                    <th>Rank</th>
-                    <th>Players</th>
-                    <th>Price</th>
-                    <th>Add to Wishlist</th>
-                    <th>Add to Owned</th>
-                </tr>
-            </thead>
-            <tbody>
-                <DiscoverTableRow games={games} />
-            </tbody>
-        </Table>
-    )
+class DiscoverTable extends React.Component {
+    state = {
+        isOpen: false,
+        modalGame: null
+    }
+    
+    openModal = (game) => {
+        this.setState({ 
+            isOpen: true,
+            modalGame: game
+        });
+    }
+
+    closeModal = () => this.setState({ isOpen: false });
+    
+    render() {
+        return (
+            <>
+                <Table striped hover className="pt-10">
+                    <thead>
+                        <tr className="text-center">
+                            <th>Image</th>
+                            <th>Title</th>
+                            <th>Rank</th>
+                            <th>Players</th>
+                            <th>Price</th>
+                            <th>More Info</th>
+                            <th>Add to Wishlist</th>
+                            <th>Add to Owned</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.games.map(game => {
+                            return <DiscoverTableRow openModal={this.openModal} game={game} key={game.id}/>
+                        })}
+                    </tbody>
+                </Table>
+
+                {this.state.isOpen 
+                    ? <GameModal 
+                        closeModal={this.closeModal} 
+                        isOpen={this.state.isOpen} 
+                        handleSubmit={this.handleSubmit}
+                        game={this.state.modalGame}
+                        /> 
+                    : null 
+                }
+            </>
+        )
+    }   
+    
 }
 
 export default DiscoverTable
--- src/components/discover/DiscoverTableRow.js
+++ src/components/discover/DiscoverTableRow.js
@@ -1,33 +1,28 @@
 import React from 'react'
 import OwnedButton from '../buttons/OwnedButton'
 import WishlistButton from '../buttons/WishlistButton'
+import InfoButton from '../buttons/InfoButton'
 
 import '../../photo_style.css'
 
 const DiscoverTableRow = (props) => {
     return (
-        props.games.map(game => {
-            if (game.visits > 100) {
-                return (
-                    <tr key={game.id} game_id={game.id} className="align-middle" >
-                        <td className="text-center"><img src={game.images.small} className="photo" alt="game" /></td>
-                        <td className="text-center">{game.name}</td>
-                        <td className="text-center">{game.rank < 1000 ? game.rank : 'N/A'}</td>
-                        <td className="text-center">{game.min_players}-{game.max_players}</td>
-                        <td className="text-center">{'$'+(game.price > 0.01 ? game.price : 'N/A')}</td>
-                        <td className="text-center">
-                            <WishlistButton game={game} />
-                        </td>
-                        <td className="text-center">
-                            <OwnedButton game={game} />
-                        </td>
-                    </tr>
-                )    
-            } else {
-                return null
-            }
-        })
-    )
+        <tr key={props.game.id} game_id={props.game.id} className="align-middle" >
+            <td className="text-center"><img src={props.game.images.small} className="photo" alt="props.game" /></td>
+            <td className="text-center">{props.game.name}</td>
+            <td className="text-center">{props.game.rank < 1000 ? props.game.rank : 'N/A'}</td>
+            <td className="text-center">{props.game.min_players}-{props.game.max_players}</td>
+            <td className="text-center">{'$'+(props.game.price > 0.01 ? props.game.price : 'N/A')}</td>
+            <td className="text-center"><InfoButton game={props.game} openModal={props.openModal} /></td>
+            <td className="text-center">
+                <WishlistButton game={props.game} />
+            </td>
+            <td className="text-center">
+                <OwnedButton game={props.game} />
+            </td>
+        </tr>
+    )    
+    
 }
 
 export default DiscoverTableRow
