2558fc7129bd03fa0671d4efe2545c2927b17ed6
Tommy Werner <wtwerner@gmail.com>
2021-05-10 15:06:30 -0500

    Adjust welcome page with new auth

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,40 +1,58 @@
-import React from 'react'
-import { Jumbotron, Container} from 'react-bootstrap'
-import RecentGames from './RecentGames'
-import { connect } from 'react-redux'
+import React from 'react';
+import { Jumbotron, Container } from 'react-bootstrap';
+import RecentGames from './RecentGames';
+import { connect } from 'react-redux';
+import { useAuth0 } from '@auth0/auth0-react';
 
-const Main = ({recentGames, popularGames, currentUser}) => {
+const Main = ({ recentGames, popularGames, currentUser }) => {
+    const { user, isAuthenticated, isLoading } = useAuth0();
+
+    const welcome = () => {
+        if (isLoading) {
+            return <div>Loading...</div>;
+        } else if (!isAuthenticated) {
+            return (
+                <div>
+                    <h1>Welcome to BG Logger!</h1>
+                    <h5>Please Login.</h5>
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                <h1>Welcome to BG Logger!</h1>
+                <h5>Logged in as {user.name}</h5>
+            </div>
+        );
+    };
 
     return (
         <>
-            <Container style={{width: "750px", padding: "30px", textAlign: "center"}}>
-                <Jumbotron className="border rounded" style={{padding: "25px"}}>
-                    {currentUser
-                        ? <h1>Welcome to BG Logger, {currentUser.data.attributes.name}</h1>
-                        : <h1>Welcome to BG Logger</h1>
-                    }
-                    <p>
-                        A simple app to search for and collect board games.
-                    </p>
-                </Jumbotron>            
+            <Container style={{ width: '750px', padding: '30px', textAlign: 'center' }}>
+                <Jumbotron className="border rounded" style={{ padding: '25px' }}>
+                    {welcome()}
+                </Jumbotron>
             </Container>
             <Container>
-                {currentUser 
-                    ? (recentGames.length > 2) ? <RecentGames games={recentGames} recent={true} /> : <RecentGames games={popularGames} recent={false} />
-                    : null
-                }
-                
+                {currentUser ? (
+                    recentGames.length > 2 ? (
+                        <RecentGames games={recentGames} recent={true} />
+                    ) : (
+                        <RecentGames games={popularGames} recent={false} />
+                    )
+                ) : null}
             </Container>
         </>
-    )
-}
+    );
+};
 
 const mapStateToProps = ({ games, currentUser }) => {
     return {
         recentGames: games.recent,
-        popularGames: games.discover.slice(0,3),
-        currentUser
-    }
-}
+        popularGames: games.discover.slice(0, 3),
+        currentUser,
+    };
+};
 
-export default connect(mapStateToProps)(Main)
+export default connect(mapStateToProps)(Main);
