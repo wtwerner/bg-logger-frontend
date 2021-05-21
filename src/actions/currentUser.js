f6d76ba00d2b0f0e1aa77a217342506233eb2a50
Tommy Werner <wtwerner@gmail.com>
2021-05-21 12:27:33 -0500

    Adjust formatting

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,109 +1,109 @@
-import { fetchGamesFromQuery, fetchGamesFromUser } from './games'
-import { fetchPlaysFromUser } from './forms'
-import { resetGames } from './games'
-import { API_ROOT } from '../apiRoot'
+import { fetchGamesFromQuery, fetchGamesFromUser } from './games';
+import { fetchPlaysFromUser } from './forms';
+import { resetGames } from './games';
+import { API_ROOT } from '../apiRoot';
 
 // Synchronous Action Creators
 
-export const setCurrentUser = user => {
-  return {
-    type: "SET_CURRENT_USER",
-    user
-  }
-}
+export const setCurrentUser = (user) => {
+    return {
+        type: 'SET_CURRENT_USER',
+        user,
+    };
+};
 
 export const clearCurrentUser = () => {
-  return {
-    type: "CLEAR_CURRENT_USER"
-  }
-}
+    return {
+        type: 'CLEAR_CURRENT_USER',
+    };
+};
 
 // Asynchronous Action Creators
 
 export const login = (credentials) => {
-    return dispatch => {
+    return (dispatch) => {
         return fetch(`${API_ROOT}/api/v1/login`, {
             credentials: 'include',
-            method: "POST",
+            method: 'POST',
             headers: {
-                "Content-Type": "application/json"
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify(credentials)
+            body: JSON.stringify(credentials),
         })
-          .then(response => response.json())
-          .then(response => {
-            if (response.error) {
-              alert(response.error)
-            } else {
-              dispatch(setCurrentUser(response))
-              dispatch(fetchGamesFromUser(response))
-              dispatch(fetchPlaysFromUser(response))
-              dispatch(fetchGamesFromQuery(''))
-            }
-        })
-          .catch(console.log)
-    }
-}
+            .then((response) => response.json())
+            .then((response) => {
+                if (response.error) {
+                    alert(response.error);
+                } else {
+                    dispatch(setCurrentUser(response));
+                    dispatch(fetchGamesFromUser(response));
+                    dispatch(fetchPlaysFromUser(response));
+                    dispatch(fetchGamesFromQuery(''));
+                }
+            })
+            .catch(console.log);
+    };
+};
 
 export const logout = () => {
-  return dispatch => {
-    return fetch(`${API_ROOT}/api/v1/logout`, {
-      credentials: 'include',
-      method: 'DELETE'
-    })
-      .then(dispatch(clearCurrentUser()))
-      .then(dispatch(resetGames()))
-  }
-}
+    return (dispatch) => {
+        return fetch(`${API_ROOT}/api/v1/logout`, {
+            credentials: 'include',
+            method: 'DELETE',
+        })
+            .then(dispatch(clearCurrentUser()))
+            .then(dispatch(resetGames()));
+    };
+};
 
 export const signup = (credentials) => {
-  return dispatch => {
-    const userInfo = {
-      user: credentials
-    }
-    return fetch(`${API_ROOT}/api/v1/signup`, {
-      credentials: "include",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(userInfo)
-    })
-      .then(r => r.json())
-      .then(response => {
-        if (response.error) {
-          alert(response.error)
-        } else {
-          dispatch(setCurrentUser(response))
-          dispatch(fetchGamesFromUser(response))
-          dispatch(fetchPlaysFromUser(response))
-          dispatch(fetchGamesFromQuery(''))
-        }
-      })
-      .catch(console.log)
-  }
-}
+    return (dispatch) => {
+        const userInfo = {
+            user: credentials,
+        };
+        return fetch(`${API_ROOT}/api/v1/signup`, {
+            credentials: 'include',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userInfo),
+        })
+            .then((r) => r.json())
+            .then((response) => {
+                if (response.error) {
+                    alert(response.error);
+                } else {
+                    dispatch(setCurrentUser(response));
+                    dispatch(fetchGamesFromUser(response));
+                    dispatch(fetchPlaysFromUser(response));
+                    dispatch(fetchGamesFromQuery(''));
+                }
+            })
+            .catch(console.log);
+    };
+};
 
 export const getCurrentUser = () => {
-  return dispatch => {
-    return fetch(`${API_ROOT}/api/v1/get_current_user`, {
-      credentials: 'include',
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      },
-    })
-      .then(r => r.json())
-      .then(response => {
-        if (response.error) {
-          console.log(response.error)
-        } else {
-          dispatch(setCurrentUser(response))
-          dispatch(fetchGamesFromUser(response))
-          dispatch(fetchPlaysFromUser(response))
-          dispatch(fetchGamesFromQuery(''))
-        }
-      })
-      .catch(console.log)
-  }
-}
+    return (dispatch) => {
+        return fetch(`${API_ROOT}/api/v1/get_current_user`, {
+            credentials: 'include',
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((r) => r.json())
+            .then((response) => {
+                if (response.error) {
+                    console.log(response.error);
+                } else {
+                    dispatch(setCurrentUser(response));
+                    dispatch(fetchGamesFromUser(response));
+                    dispatch(fetchPlaysFromUser(response));
+                    dispatch(fetchGamesFromQuery(''));
+                }
+            })
+            .catch(console.log);
+    };
+};
