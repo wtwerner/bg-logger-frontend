fd6402baaf0da942c4ebcf41520e1656c4cf3905
Tommy Werner <wtwerner@gmail.com>
2021-05-21 11:47:48 -0500

    Adjust formatting

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,45 +1,58 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { updateLoginForm, resetLoginForm } from "../../actions/forms.js"
-import { login } from "../../actions/currentUser.js"
-import { Form, Button } from 'react-bootstrap'
-import { useHistory} from 'react-router-dom'
+import React from 'react';
+import { connect } from 'react-redux';
+import { updateLoginForm, resetLoginForm } from '../../actions/forms.js';
+import { login } from '../../actions/currentUser.js';
+import { Form, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
 const Login = ({ loginFormData, updateLoginForm, resetLoginForm, login }) => {
+    const history = useHistory();
 
-    const history = useHistory()
-    
-    const handleOnChange = event => {
-        const { name, value } = event.target
+    const handleOnChange = (event) => {
+        const { name, value } = event.target;
         const updatedFormInfo = {
             ...loginFormData,
-            [name]: value
-        }
-        updateLoginForm(updatedFormInfo)
-    }
+            [name]: value,
+        };
+        updateLoginForm(updatedFormInfo);
+    };
 
-    const handleOnSubmit = event => {
-        event.preventDefault()
-        login(loginFormData)
-        resetLoginForm()
-        history.push('/')
-    }
+    const handleOnSubmit = (event) => {
+        event.preventDefault();
+        login(loginFormData);
+        resetLoginForm();
+        history.push('/');
+    };
 
     return (
-        <div style={{width: "300px"}}>
+        <div style={{ width: '300px' }}>
             <Form onSubmit={handleOnSubmit}>
-                <Form.Control value={loginFormData.email} placeholder="email" name="email" type="text" onChange={handleOnChange} />
-                <Form.Control value={loginFormData.password} placeholder="password" name="password" type="password" onChange={handleOnChange} />
-                <Button style={{width: "300px"}} value="Log In" type="submit">Login</Button>
+                <Form.Control
+                    value={loginFormData.email}
+                    placeholder="email"
+                    name="email"
+                    type="text"
+                    onChange={handleOnChange}
+                />
+                <Form.Control
+                    value={loginFormData.password}
+                    placeholder="password"
+                    name="password"
+                    type="password"
+                    onChange={handleOnChange}
+                />
+                <Button style={{ width: '300px' }} value="Log In" type="submit">
+                    Login
+                </Button>
             </Form>
         </div>
-    )
-}
+    );
+};
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
-        loginFormData: state.forms.login
-    }
-}
+        loginFormData: state.forms.login,
+    };
+};
 
-export default connect(mapStateToProps, { updateLoginForm, resetLoginForm, login })(Login)
+export default connect(mapStateToProps, { updateLoginForm, resetLoginForm, login })(Login);
