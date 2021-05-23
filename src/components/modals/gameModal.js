e8fa6030e8912056645bb0ba4033104e3d003076
Tommy Werner <wtwerner@gmail.com>
2021-05-23 16:48:49 -0500

    Remove BGG url due to API change

--- src/components/modals/gameModal.js
+++ src/components/modals/gameModal.js
@@ -1,34 +1,36 @@
-import React from 'react'
-import { Modal, Container, Row, Col } from 'react-bootstrap'
-import './modal.css'
-import '../../photo_style.css'
+import React from 'react';
+import { Modal, Container, Row, Col } from 'react-bootstrap';
+import './modal.css';
+import '../../photo_style.css';
 
 export default class GameModal extends React.Component {
-
     starRating = (rating) => {
-        if (0 < rating && rating< 1.5) {
-            return <span>⭐</span>
+        if (0 < rating && rating < 1.5) {
+            return <span>⭐</span>;
         } else if (1.5 < rating && rating < 2.5) {
-            return <span>⭐⭐</span>
+            return <span>⭐⭐</span>;
         } else if (2.5 < rating && rating < 3.5) {
-            return <span>⭐⭐⭐</span>
+            return <span>⭐⭐⭐</span>;
         } else if (3.5 < rating && rating < 4.5) {
-            return <span>⭐⭐⭐⭐</span>
+            return <span>⭐⭐⭐⭐</span>;
         } else if (4.5 < rating && rating < 5) {
-            return <span>⭐⭐⭐⭐⭐</span>
+            return <span>⭐⭐⭐⭐⭐</span>;
         }
-    }
+    };
 
-    render(){
-        return(
-            <Modal 
-                show={this.props.isOpen} 
-                onHide={this.props.closeModal} 
+    render() {
+        return (
+            <Modal
+                show={this.props.isOpen}
+                onHide={this.props.closeModal}
                 dialogClassName="modal-90w"
                 aria-labelledby="example-custom-modal-styling-title"
             >
                 <Modal.Header closeButton>
-                    <Modal.Title>{this.props.game.name} ({this.props.game.year_published}) - {this.props.game.primary_designer.name}</Modal.Title>
+                    <Modal.Title>
+                        {this.props.game.name} ({this.props.game.year_published}) -{' '}
+                        {this.props.game.primary_designer.name}
+                    </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Container>
@@ -38,17 +40,21 @@ export default class GameModal extends React.Component {
                             </Col>
                             <Col xs={6} md={4}>
                                 Rank: {this.props.game.rank}
-                                <br/>
+                                <br />
                                 Players: {this.props.game.min_players}-{this.props.game.max_players}
-                                <br/>
+                                <br />
                                 Play Time: {this.props.game.min_playtime}-{this.props.game.max_playtime} minutes
-                                <br/>
-                                Rating: {this.starRating(this.props.game.average_user_rating)} ({this.props.game.num_user_ratings} reviews)
-                                <br/>
-                                <a target="_blank" rel="noreferrer" href={`https://boardgamegeek.com/boardgame/${this.props.game.bgg_id}`}>View on BoardGameGeek</a>
+                                <br />
+                                Rating: {this.starRating(this.props.game.average_user_rating)} (
+                                {this.props.game.num_user_ratings} reviews)
+                                <br />
+                                {console.log(this.props.game)}
+                                <a target="_blank" rel="noreferrer" href={this.props.game.url}>
+                                    View on Board Game Atlas
+                                </a>
                             </Col>
                         </Row>
-                        <br/>
+                        <br />
                         <Row>
                             <Col>
                                 <h6>Description:</h6>
@@ -58,6 +64,6 @@ export default class GameModal extends React.Component {
                     </Container>
                 </Modal.Body>
             </Modal>
-        )
+        );
     }
 }
